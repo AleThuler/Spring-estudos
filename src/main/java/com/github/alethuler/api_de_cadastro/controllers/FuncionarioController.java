132d85f51dec4280d9003370a0618e1a29907119
AleThuler <alesthuler@gmail.com>
2026-09-10 22:46:03 -0300

    Criado a entidade funcionario e seu controller, além de atualizar a lista de dependencias

--- src/main/java/com/github/alethuler/api_de_cadastro/controllers/FuncionarioController.java
+++ src/main/java/com/github/alethuler/api_de_cadastro/controllers/FuncionarioController.java
@@ -1,4 +1,4 @@
-package com.github.alethuler.api_de_cadastro;
+package com.github.alethuler.api_de_cadastro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class FuncionarioController {
 
     @GetMapping("/java")
     public String falarJava(){
