c806a0faf83aab69475b8cd08a415f268c4c6926
AleThuler <alesthuler@gmail.com>
2026-09-10 21:45:27 -0300

    Adicionando a classe Controller

--- src/main/java/com/github/alethuler/api_de_cadastro/Controller.java
+++ src/main/java/com/github/alethuler/api_de_cadastro/Controller.java
@@ -1,4 +1,15 @@
 package com.github.alethuler.api_de_cadastro;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/java")
+    public String falarJava(){
+        return "Java!";
+    }
 }
